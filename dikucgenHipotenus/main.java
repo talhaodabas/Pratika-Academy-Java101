eb25fa5cf008e1e2f34a34cf646b461b488e14d8
Talha Odabaş <149323113+talhaodabas@users.noreply.github.com>
2025-08-04 20:07:48 +0300

    Update main.java

--- dikucgenHipotenus/main.java
+++ dikucgenHipotenus/main.java
@@ -4,13 +4,19 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[]args){
-        int a,b;
-        double c;
+        int a,b,c;
+        double u,alan;
         Scanner input = new Scanner(System.in);
-        System.out.println("Yukardaki üçgenin c kenarının uzunluğunu bulmak için sırayla a ve b kenarların uzunluklarını girin.");
+        System.out.println("Üçgenin alanını bulmak için sırayla düz kenarların uzunluklarını girin.");
         a = input.nextInt();
         b = input.nextInt();
-        c = Math.sqrt((a*a)+(b*b));
-        System.out.println("C = "+c);
+        c = input.nextInt();
+
+        u = (a+b+c)/2;
+        alan = Math.sqrt(u * (u-a)*(u-b)*(u-c));
+
+        System.out.println("Üçgenin alanı : "+alan);
     }
 }
+
+
